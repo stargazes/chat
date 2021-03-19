4b4d7616d200f4c82960377430f78b5ef62a7442
HOFAN-SZ\liyuanhua <549467266@qq.com>
2021-03-19 17:38:06 +0800

    no message

--- controller/api_room.go
+++ controller/api_room.go
@@ -24,16 +24,15 @@ func CreateRoom(c *gin.Context) {
 
 		go func() {
 
-			models.CreateRoom(&room)//创建MySQL记录
+			models.CreateRoom(&room) //创建MySQL记录
 
-			tools.AddChatRoom(room.RoomHash,room.UserId)
+			tools.AddChatRoom(room.RoomHash, room.UserId)
 		}()
 
 		c.JSON(http.StatusOK, gin.H{
 			"status": tools.SUCCESS,
 			"msg":    "创建成功",
 		})
-
 
 	} else {
 
@@ -44,5 +43,31 @@ func CreateRoom(c *gin.Context) {
 		})
 
 	}
+
+}
+
+type AddForm struct {
+	roomHash string `from:"roomHash"`
+	userId   int64  `from:"userId"`
+}
+
+func AddRoom(c *gin.Context) {
+
+	var addForm AddForm
+	if c.ShouldBind(&addForm) != nil {
+		//
+		c.JSON(http.StatusOK, gin.H{
+			"status": tools.FAIL,
+			"msg":    "参数有误",
+		})
+		c.Abort()
+	}
+
+	tools.AddChatRoom(addForm.roomHash,addForm.userId)
+	
+	c.JSON(http.StatusOK, gin.H{
+		"status": tools.SUCCESS,
+		"msg":    "加入成功",
+	})
 
 }
